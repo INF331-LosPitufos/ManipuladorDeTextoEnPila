2f19bd4df7ab34ded2049ed2f0ff260e579f6115
Victor <sproutvmc@gmail.com>
2022-09-06 18:10:14 -0300

    clase stack actualizada

--- programa-victor/stack.py
+++ programa-victor/stack.py
@@ -11,26 +11,36 @@ class Stack:
         """ Agrega el elemento x a la pila. """
         # Apilar es agregar al final de la lista.
         self.items.append(x)
+        print("Texto ingresado exitosamente!")
 
     def pop(self):
         """ Devuelve el elemento tope y lo elimina de la pila.
             Si la pila está vacía levanta una excepción. """
         try:
+            print("Elemento eliminado exitosamente")
             return self.items.pop()
         except IndexError:
             raise ValueError("La pila está vacía")
 
     def is_empty(self):
-        """ Devuelve True si la lista está vacía, False si no. """
-        return self.items == []
+        """ Devuelve True si la lista está vacía, False si no. """        
+        if( self.items == [] ): 
+            print("Pila actualmente vacía!")
+            return True
+        return False
+
     
     def longerText(self):
         """ Retorna el texto más largo de la pila """
-        return max(self.items, key=len)
+        lt = max(self.items, key=len)
+        print(lt)
+        return lt
     
     def smallerText(self):
         """ Retorna el texto más pequeño de la pila """
-        return min(self.items, key=len)
+        st = min(self.items, key=len)
+        print(st)
+        return st
 
     def printText(self, i):
         """ Imprime el texto en la posicion i de la pila """
@@ -41,16 +51,13 @@ class Stack:
     
     def compareSizes(self, i, j):
         """ Retorna un número que representa la diferencia en tamaño de ambas palabras.
-            i, j representan los indeices ed las palabras a comparar en la pila"""
+            i, j representan los indices de las palabras a comparar en la pila"""
         try:
             return abs(self.items[i] - self.items[j])
         except IndexError:
             raise ValueError("Indice ingresado no es válido. Parte desde 0")
+    
+    def printStack(self):
+        print("Contenido actual de la pila")
+        print(self.items)
 
-"""
-Que permita almacenar un texto en una pila.
-Que dé al usuario la posibilidad de:
-Ver cuál es el texto más largo en la pila y el más corto.
-Imprimir cualquier texto de la pila en cualquier momento.
-Permitir comparar tamaños.
-"""
